1bfc139ea72c6c7b003ad850573ba77cef89e1f6
DoomTaper <ayush.singh.ece13@iitbhu.ac.in>
2016-09-03 10:50:59 -0400

    arachni xml+json parser without http_parse argument

--- ptp/ptp.py
+++ ptp/ptp.py
@@ -11,7 +11,7 @@ import warnings
 
 from .libptp.exceptions import NotSupportedToolError, NotSupportedVersionError
 from .libptp.constants import UNKNOWN, RANKING_SCALE
-from .tools.arachni.parser import ArachniXMLParser
+from .tools.arachni.parser import ArachniXMLParser, ArachniJSParser
 from .tools.skipfish.parser import SkipfishJSParser
 from .tools.w3af.parser import W3AFXMLParser
 from .tools.wapiti.parser import WapitiXMLParser, Wapiti221XMLParser
--- ptp/tools/arachni/parser.py
+++ ptp/tools/arachni/parser.py
@@ -12,7 +12,7 @@ from lxml.etree import XMLSyntaxError
 
 from ptp.libptp import constants
 from ptp.libptp.exceptions import NotSupportedVersionError
-from ptp.libptp.parser import XMLParser
+from ptp.libptp.parser import XMLParser, JSONParser
 
 
 class ArachniXMLParser(XMLParser):
@@ -23,7 +23,8 @@ class ArachniXMLParser(XMLParser):
     __version__ = (
         r'(^0\.4\.[6-7]{1}$)|'
         r'(^1\.0(\.[1-6]{1})?$)|'
-        r'(^1\.1$)')
+        r'(^1\.1$)|'
+        r'(^1\.2\.1)')
 
     HIGH = 'high'
     MEDIUM = 'medium'
@@ -91,6 +92,27 @@ class ArachniXMLParser(XMLParser):
         else:
             raise NotSupportedVersionError('PTP does NOT support this version of Arachni.')
 
+    def get_data(self, tree):
+        """XML file itself contains all requests and responses just needed to parse it correctly
+        That is what this function does and return a list of dicts. HTTP traffic is divided into following fields
+        * request
+        * response status code
+        * response headers
+        * response body
+        """
+        data = []
+        for i in range(len(tree)):
+            t_req = tree[i][1]
+            t_res = tree[i][2]
+            # using max() function to get empty string if request body is None
+            data.append({
+                'request': t_req.find('.//raw').text + max(t_req.find('.//body').text, '') + '\n',
+                'response_status_code': t_res.find('.//code').text,
+                'response_headers': t_res.find('.//raw_headers').text.strip(),
+                'response_body': t_res.find('.//body').text.strip()
+            })
+        return data
+
     def parse_report(self):
         """Parse the results of the report.
 
@@ -101,4 +123,110 @@ class ArachniXMLParser(XMLParser):
         self.vulns = [
             {'ranking': self.RANKING_SCALE[vuln.find('.//severity').text.lower()]}
             for vuln in self.stream.find('.//issues')]
+        temp = []
+        for record in self.stream.xpath('//variations//variation//referring_page'):
+            temp.append(record.getchildren())
+        self.vulns.append({'transactions': self.get_data(temp)})
+        return self.vulns
+
+
+class ArachniJSParser(JSONParser):
+    """Arachni XML specialized parser."""
+
+    __tool__ = 'arachni'
+    __format__ = 'xml'
+    __version__ = (
+        r'(^0\.4\.[6-7]{1}$)|'
+        r'(^1\.0(\.[1-6]{1})?$)|'
+        r'(^1\.1$)|'
+        r'(^1\.2\.1)')
+
+    HIGH = 'high'
+    MEDIUM = 'medium'
+    LOW = 'low'
+    INFO = 'informational'
+
+    RANKING_SCALE = {
+        HIGH: constants.HIGH,
+        MEDIUM: constants.MEDIUM,
+        LOW: constants.LOW,
+        INFO: constants.INFO}
+
+    def __init__(self, pathname, filename='*.json', first=True):
+        """Initialize ArachniXMLParser.
+        :param str pathname: Path to the report directory.
+        :param str filename: Regex matching the report file.
+        :param bool first: Only process first file (``True``) or each file that matched (``False``).
+        """
+        JSONParser.__init__(self, pathname, filename, first=first)
+
+    @classmethod
+    def is_mine(cls, pathname, filename='*.json', first=True):
+        """Check if it can handle the report file.
+        :param str pathname: Path to the report directory.
+        :param str filename: Regex matching the report file.
+        :param bool first: Only process first file (``True``) or each file that matched (``False``).
+        :return: `True` if it supports the report, `False` otherwise.
+        :rtype: :class:`bool`
+        """
+        try:
+            stream = cls.handle_file(pathname, filename, first=first)
+        except (IOError, TypeError, XMLSyntaxError):
+            return False
+        if stream.has_key('version'):
+            version = stream['version']
+        else:
+            return False
+        if not re.findall(cls.__version__, version, re.IGNORECASE):
+            return False
+        return True
+
+    def parse_metadata(self):
+        """Parse the metadata of the report.
+        :raises: :class:`NotSupportedVersionError` -- if it does not support the version of this report.
+        :return: The metadata of the report.
+        :rtype: dict
+        """
+        # Find the version of Arachni.
+        version = self.stream['version']
+        # Reconstruct the metadata
+        # TODO: Retrieve the other metadata likes the date, etc.
+        self.metadata = {'version': version}
+        if self.check_version(self.metadata):
+            return self.metadata
+        else:
+            raise NotSupportedVersionError('PTP does NOT support this version of Arachni.')
+
+    def get_data(self, issues):
+        """JSON file itself contains all requests and responses just needed to parse it correctly
+        That is what this function does and return a list of dicts. HTTP traffic is divided into following fields
+        * request
+        * response status code
+        * response headers
+        * response body
+        """
+        data = []
+        for issue in issues:
+            for variation in issue['variations']:
+                # using max() function to get empty string if request body is None
+                data.append({
+                    'request': variation['request']['headers_string']+max(variation['request']['body'], '')+'\n',
+                    'response status code': variation['response']['code'],
+                    'response headers': variation['response']['headers_string'],
+                    'response body': variation['response']['body']
+                })
+        return data
+
+    def parse_report(self, full_parse):
+        """Parse the results of the report.
+        :return: List of dicts where each one represents a discovery.
+        :rtype: :class:`list`
+        """
+        self.vulns = [
+            {'ranking': self.RANKING_SCALE[vuln['severity'].lower()]}
+            for vuln in self.stream['issues']]
+
+        if full_parse:
+            self.data = self.get_data(self.stream['issues'])
+            return self.vulns, self.data
         return self.vulns
